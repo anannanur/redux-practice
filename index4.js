c5ae6983d95457517f8ca2fe424777e2002c3561
anannanur <anannaanu974@gmail.com>
2022-07-19 16:43:33 +0600

    multiple reducer  combining

--- index4.js
+++ index4.js
@@ -1,13 +1,20 @@
-const { createStore } = require("redux");
+const { createStore, combineReducers } = require("redux");
+// product 
 const GET_PRODUCTS = 'GET_PRODUCTS';
 const ADD_PRODUCTS = 'ADD_PRODUCTS';
 
+// cart 
+const GET_CART = 'GET_CART';
+const ADD_CART = 'ADD_CART';
+
 // state
 const products = {
-
     product_items: ["sugar", "salt"],
     numberOfProducts: 2
-
+}
+const cart = {
+    cart_items: ["sugar"],
+    numberOfItem: 1
 }
 
 // action
@@ -19,6 +26,17 @@ const getProducts = () => {
 const addProducts = (value) => {
     return {
         type: ADD_PRODUCTS,
+        payload: value
+    }
+}
+const getCart = () => {
+    return {
+        type: GET_CART
+    }
+}
+const addCart = (value) => {
+    return {
+        type: ADD_CART,
         payload: value
     }
 }
@@ -39,15 +57,44 @@ const productReducer = (state = products, action) => {
             }
         
         default:
-            state
+            return state
     }
 
 }
 
-const store = createStore(productReducer);
+const cartReducer = (state = cart, action) => {
+
+    switch (action.type) {
+        case GET_CART:
+            return{
+               ...state
+            }
+        case ADD_CART:
+            return{
+                    cart_items : [...state.cart_items, action.payload],
+                    numberOfItem : state.numberOfItem + 1
+               
+            }
+        
+        default:
+            return state
+    }
+
+}
+
+const rootReducer = combineReducers({
+    productR: productReducer,
+    cartR: cartReducer
+})
+
+// store
+const store = createStore(rootReducer);
 store.subscribe(()=>{
     console.log(store.getState())
 })
+
 store.dispatch(getProducts())
-store.dispatch(addProducts("chips"))
 store.dispatch(addProducts("potato"))
+
+store.dispatch(getCart())
+store.dispatch(addCart("chips"))
